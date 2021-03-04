09be152a831906fb9d66f1fd40a808cd38af0fd7
bariisarslans <bariisarslans@hotmail.com>
2021-03-04 23:24:21 +0300

    rmc config

--- App.js
+++ App.js
@@ -1,72 +1,132 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
+  Button,
   View,
-  Text,
-  StatusBar,
+  ActivityIndicator,
+  Platform
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { addEventListener, removeEventListener, requestPermissions, EuroMessageApi, VisilabsApi, setApplicationIconBadgeNumber, logToConsole, RDStoryView } from 'react-native-related-digital'
 
-const App: () => React$Node = () => {
+const App = () => {
+  const [loading, setLoading] = useState(false)
+
+  const appAlias = Platform.OS === "ios" ? "RelatedStoreIOS" : "RelatedStoreAndroid";
+
+  const siteId = "356467332F6533766975593D";
+  const organizationId = "676D325830564761676D453D";
+  const dataSource = "visistore";
+
+  const euroMessageApi = new EuroMessageApi(appAlias)
+  const visilabsApi = new VisilabsApi(appAlias, siteId, organizationId, dataSource)
+
+  useEffect(() => {
+    logToConsole(true)
+
+    addExtra()
+    addListeners()
+
+    return () => removeListeners()
+  }, [])
+
+  const addListeners = () => {
+
+    addEventListener('register', async (token) => {
+      const subscribeResult = await euroMessageApi.subscribe(token)
+
+      visilabsApi.register(token, (result) => {
+        
+      })
+    }, (notificationPayload) => {
+      console.log('notification payload', notificationPayload)
+    }, euroMessageApi, visilabsApi)
+
+    addEventListener('registrationError', async (registrationError) => {
+      console.log('registrationError is ', registrationError)
+    }, euroMessageApi)
+  }
+
+  const addExtra = async () => {
+    await euroMessageApi.setUserProperty('extra', 1)
+  }
+
+  const setBadgeNumber = () => {
+    const number = 3
+    setApplicationIconBadgeNumber(number)
+  }
+
+  const sendCustomEvent = () => {
+    visilabsApi.customEvent('*', {
+      'id': '1',
+      'name': 'Product Name'
+    })
+  }
+
+  const getRecommendations = async () => {
+    try {
+      const zoneId = '6'
+      const productCode = ''
+
+      const recommendations = await visilabsApi.getRecommendations(zoneId, productCode)
+      console.log('recommendations', recommendations)
+    }
+    catch (e) {
+      console.log('recommendations error', e)
+    }
+  }
+
+  const removeListeners = () => {
+    removeEventListener('register')
+    removeEventListener('registrationError')
+  }
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        {
+          loading ?
+          <ActivityIndicator 
+            size='large'
+            animating={loading} /> :
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <RDStoryView
+              // actionId={'1'} // optional
+              onItemClicked={(data) => {
+                console.log('Story data', data)
+              }}
+              style={{ flex: 1 }}
+            />
+            <Button 
+              title='REQUEST PERMISSONS'
+              onPress={() => {
+                requestPermissions()
+              }} 
+            />
+            <Button 
+              title='SET BADGE NUMBER TO 3 (IOS)'
+              onPress={() => {
+                setBadgeNumber()
+              }} 
+            />
+            <Button 
+              title='SEND CUSTOM EVENT'
+              onPress={() => {
+                sendCustomEvent()
+              }} 
+            />
+            <Button
+              title='GET RECOMMENDATIONS'
+              onPress={async () => {
+                await getRecommendations()
+              }}
+            />
+          </ScrollView>
+        }
       </SafeAreaView>
     </>
   );
@@ -74,41 +134,12 @@ const App: () => React$Node = () => {
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: Colors.lighter,
+    backgroundColor: '#FFF',
+    padding: 20
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  divider: {
+    height: 20
+  }
 });
 
 export default App;
